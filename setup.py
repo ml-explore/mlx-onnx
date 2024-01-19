9516482d9f1b4638cd3426b78faa81b3a91955ef
dc-dc-dc <dgcruz983@gmail.com>
2024-01-19 11:20:08 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="mlx-onnx",
+    version="0.0.1",
+    description="MLX backend for onnx",
+    install_requires=["mlx", "onnx"],
+    extras_require={
+        "test": ["numpy", "pytest"],
+    },
+    packages=["mlx.onnx"],
+)
